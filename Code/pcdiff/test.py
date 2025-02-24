c4e0d4998e8303e0cb48535941e4234b17bab621
MathiasBrun <aagaard.mathias@gmail.com>
2025-02-24 08:24:54 +0100

    Create test.py

---
+++ Code/pcdiff/test.py
@@ -0,0 +1,28 @@
+import numpy as np
+from pcdiff import knn_graph, estimate_basis, build_grad_div, laplacian
+
+data = np.loadtxt('C:/Users/aagaa/Documents/GitHub/R-D/Code/Leihui Code/src/data/bunny.xyz')
+pos = data[:, 0:3]
+# Random point cloud
+#pos = np.random.rand(1000, 3)
+
+# Generate kNN graph
+edge_index = knn_graph(pos, 20)
+# Estimate normals and local frames
+basis = estimate_basis(pos, edge_index)
+# Build gradient and divergence operators (Scipy sparse matrices)
+grad, div = build_grad_div(pos, *basis, edge_index)
+
+# Setup the Laplacian as the divergence of gradient:
+laplacian = -(div @ grad)
+
+# Define some function on the point cloud
+x = np.random.rand(np.size(pos,0), 1)
+
+# Compute gradient of function
+# The output is of size 2N, with the two components of the vector field interleaved:
+# [x_1, y_1, x_2, y_2, ..., x_N, y_N]
+grad_x = grad @ x
+
+print('Gradients:\n', grad_x,
+      '\n\n Number of points: ', np.size(grad_x,0))
