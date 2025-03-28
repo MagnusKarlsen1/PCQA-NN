2bf781b83cbe445077bb3efab446a16bb991283b
MagnusKarlsen1 <92080473+MagnusKarlsen1@users.noreply.github.com>
2025-03-28 09:27:46 +0100

    sf

---
+++ Code/Gradient NN/src/datagenerator.py
@@ -0,0 +1,63 @@
+
+
+
+
+
+
+
+
+
+
+def create_data():
+    
+    
+    # Create shape
+    
+    
+    # Create pointcloud
+    
+    
+    # Optionals: 
+        #  Add noise
+    
+    
+        # Create holes
+    
+    # Calculate point density
+    
+    # Find edges 
+    
+        
+    # Find neighbors
+
+        
+        
+        # Calculate curvature
+        
+        
+        
+        # Calculate edge number based on neighbors
+    
+    # Save all data in a np.array
+    
+    # Save to txt file
+    
+    return 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
