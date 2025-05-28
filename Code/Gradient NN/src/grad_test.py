dbf1d7e5153c86a8c801c83727b28b52f000b70f
MathiasBrun <aagaard.mathias@gmail.com>
2025-05-28 15:07:05 +0200

    1
    1

--- Code/Gradient NN/src/grad_test.py
+++ Code/Gradient NN/src/grad_test.py
@@ -20,9 +20,12 @@ import geometric_functions as gf
 import meshlab_functions as mf
 
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 path = "C:/Users/magnu/OneDrive/Dokumenter/GitHub/R-D/Code/Leihui Code/dataset/SelfGeneratedClouds/Chain_wheel_05.xyz"
 path_in = "C:/Users/magnu/OneDrive/Dokumenter/GitHub/R-D/Drawings/Chain_whee.STL"
 =======
+=======
+>>>>>>> Stashed changes
 path = "C:/Users/aagaa/OneDrive - Aarhus universitet/Dokumenter/GitHub/R-D/Code/Leihui Code/dataset/SelfGeneratedClouds/Chain_wheel1mm.xyz"
 path_in = "C:/Users/aagaa/OneDrive - Aarhus universitet/Dokumenter/GitHub/R-D/Drawings/Chain_whee.STL"
 >>>>>>> Stashed changes
@@ -47,8 +50,13 @@ header_label =["Label"]
 header = ["edge_mean", "plane_mean", "curvature", "linearity", "planarity", "omnivaraiance", "eigensum", "AverageRadius", "PointsInside", "grad_dist"]
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 feature_PATH = os.path.abspath(os.path.join(BASE_DIR, f"../Data/Training_data/testcloud_feat_05.txt"))
 label_PATH = os.path.abspath(os.path.join(BASE_DIR, f"../Data/Training_data/testcloud_lab_05.txt"))
+=======
+feature_PATH = os.path.abspath(os.path.join(BASE_DIR, f"../Data/Training_data/testcloud_feat.txt"))
+label_PATH = os.path.abspath(os.path.join(BASE_DIR, f"../Data/Training_data/testcloud_lab.txt"))
+>>>>>>> Stashed changes
 =======
 feature_PATH = os.path.abspath(os.path.join(BASE_DIR, f"../Data/Training_data/testcloud_feat.txt"))
 label_PATH = os.path.abspath(os.path.join(BASE_DIR, f"../Data/Training_data/testcloud_lab.txt"))
